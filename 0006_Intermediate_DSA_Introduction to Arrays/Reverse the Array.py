f23a567eed4b208f0fdea7ed14d019cf601fd1cb
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2025-04-06 10:43:33 +0530

    Add solution to reverse an array

---
+++ 0006_Intermediate_DSA_Introduction to Arrays/Reverse the Array.py
@@ -0,0 +1,81 @@
+"""
+
+Problem Description
+
+You are given a constant array A.
+
+You are required to return another array which is the reversed form of the input array.
+
+
+
+Problem Constraints
+
+1 <= A.size() <= 10000
+
+1 <= A[i] <= 10000
+
+
+
+Input Format
+
+First argument is a constant array A.
+
+
+
+Output Format
+
+Return an integer array.
+
+
+
+Example Input
+
+Input 1:
+
+A = [1,2,3,2,1]
+Input 2:
+
+A = [1,1,10]
+
+
+Example Output
+
+Output 1:
+
+ [1,2,3,2,1]
+Output 2:
+
+ [10,1,1]
+
+
+Example Explanation
+
+Explanation 1:
+
+Reversed form of input array is same as original array
+Explanation 2:
+
+Clearly, Reverse of [1,1,10] is [10,1,1]
+"""
+
+class Solution:
+    # @param A : tuple of integers
+    # @return a list of integers
+    def solve(self, A):
+        B=[]
+        for i in range (len(A)-1,-1,-1):
+            B.append(A[i])
+
+        return B
+
+#      return A[::-1]
+
+# Testing the function
+A = [1, 2, 3, 2, 1]
+solution = Solution()
+result = solution.solve(A)
+print(result)  # Output: [1, 2, 3, 2, 1]
+A = [1, 1, 10]
+solution = Solution()
+result = solution.solve(A)
+print(result)  # Output: [10, 1, 1]
