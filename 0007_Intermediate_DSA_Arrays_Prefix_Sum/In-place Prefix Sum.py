1042046ff0b1345d1ab8784c9a4d87e600cb88b7
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2025-04-07 22:07:00 +0530

    Add solution for in-place prefix sum calculation in an array

---
+++ 0007_Intermediate_DSA_Arrays_Prefix_Sum/In-place Prefix Sum.py
@@ -0,0 +1,74 @@
+"""
+
+Problem Description
+
+Given an array A of N integers. Construct prefix sum of the array in the given array itself.
+
+
+Problem Constraints
+
+1 <= N <= 105
+1 <= A[i] <= 103
+
+
+Input Format
+
+Only argument A is an array of integers.
+
+
+Output Format
+
+Return an array of integers denoting the prefix sum of the given array.
+
+
+Example Input
+
+Input 1:
+
+A = [1, 2, 3, 4, 5]
+Input 2:
+
+A = [4, 3, 2]
+
+
+Example Output
+
+Output 1:
+
+[1, 3, 6, 10, 15]
+Output 2:
+
+[4, 7, 9]
+
+
+Example Explanation
+
+Explanation 1:
+
+The prefix sum array of [1, 2, 3, 4, 5] is [1, 3, 6, 10, 15].
+Explanation 2:
+
+The prefix sum array of [4, 3, 2] is [4, 7, 9].
+"""
+
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def solve(self, A):
+        sum=0
+        for i in range(len(A)):
+            sum+=A[i]
+            A[i]=sum
+        return A
+
+
+# Test cases
+if __name__ == "__main__":
+    # Test case 1
+    A = [1, 2, 3, 4, 5]
+    solution = Solution()
+    print(solution.solve(A))  # Output: [1, 3, 6, 10, 15]
+
+    # Test case 2
+    A = [4, 3, 2]
+    print(solution.solve(A))  # Output: [4, 7, 9]
