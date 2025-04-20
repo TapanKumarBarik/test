db1abce6bf94a6f370c961ac7934711e2fedf461
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2025-04-20 11:45:09 +0530

    Add problem statement and solution for converting a number from any base to decimal

---
+++ 0014_Intermediate_DSA_Bit Manipulations_1/Any base to decimal.py
@@ -0,0 +1,79 @@
+"""
+Problem Description
+
+You are given a number A. You are also given a base B. A is a number on base B.
+You are required to convert the number A into its corresponding value in decimal number system.
+
+
+Problem Constraints
+
+0 <= A <= 109
+2 <= B <= 9
+
+
+Input Format
+
+First argument A is an integer.
+Second argument B is an integer.
+
+
+Output Format
+
+Return an integer.
+
+
+Example Input
+
+Input 1:
+A = 1010
+B = 2
+Input 2:
+A = 22
+B = 3
+
+
+Example Output
+
+Output 1:
+10
+Output 2:
+8
+
+
+Example Explanation
+
+For Input 1:
+The decimal 10 in base 2 is 1010.
+For Input 2:
+The decimal 8 in base 3 is 22.
+
+
+"""
+
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+        res=0
+
+        multiplier = 1
+        while A>0:
+            temp=A%10 * multiplier
+            multiplier*= B
+            A=A//10
+            res+=temp
+
+        return res
+
+
+# Test the function
+if __name__ == "__main__":
+    sol = Solution()
+    A = 1010
+    B = 2
+    print(sol.solve(A, B))  # Output: 10
+
+    A = 22
+    B = 3
+    print(sol.solve(A, B))  # Output: 8
